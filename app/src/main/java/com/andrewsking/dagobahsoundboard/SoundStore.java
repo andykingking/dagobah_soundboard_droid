2945f3ac02c26a4fbc09cb446c71b020df41a562
Andrew King <me@andrewsking.com>
2017-06-23 15:21:51 +1000

    Add Rejoice sound

--- app/src/main/java/com/andrewsking/dagobahsoundboard/SoundStore.java
+++ app/src/main/java/com/andrewsking/dagobahsoundboard/SoundStore.java
@@ -13,6 +13,7 @@ public class SoundStore {
             new Sound(R.raw.kick_it, "Kick It"),
             new Sound(R.raw.noooo, "Noooo"),
             new Sound(R.raw.push_it, "Push It"),
+            new Sound(R.raw.rejoice, "Rejoice"),
             new Sound(R.raw.roar, "Roar"),
             new Sound(R.raw.shame, "Shame"),
             new Sound(R.raw.victory, "Victory")
